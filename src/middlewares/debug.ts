17623f068d380a41bf778a20f91f507c3556b4d3
Felipe Barcelos <felipebarcelospro@MacBook-Pro-de-Felipe.local>
2024-07-16 18:55:01 -0300

    feat: add middleware to debug

--- src/middlewares/debug.ts
+++ src/middlewares/debug.ts
@@ -1,8 +1,13 @@
 import { NextFunction, Request, Response } from 'npm:@types/express@4.17.15';
 
 export const debugMiddleware = (req: Request, _: Response, next: NextFunction) => {
-  console.log('Request - x-sender-channel:', req.headers['x-sender-channel'] as string);
-  console.log('Request - x-sender-name:', req.headers['x-sender-name'] as string);
-  console.log('Request - x-user-id:', req.headers['x-user-id'] as string);
+  console.log({
+    path: req.path,
+    method: req.method,
+    'x-sender-channel': req.headers['x-sender-channel'] as string,
+    'x-sender-name': req.headers['x-sender-name'] as string,
+    'x-user-id': req.headers['x-user-id'] as string,
+  });
+  
   next();
 };
