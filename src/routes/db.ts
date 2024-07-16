3e21b3298517d0357bf9d8fef97023ede4bd58c3
Felipe Barcelos <felipebarcelospro@MacBook-Pro-de-Felipe.local>
2024-07-16 12:48:51 -0300

    feat: add route to clean db

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { kv } from '../config/kv.ts';
 import { Request, Response } from 'express';
+import { Appointment, Contact, Service, User } from '../types.ts';
 
 const dbRouter = Router();
 
@@ -11,7 +12,7 @@ const dbRouter = Router();
  * @returns {object} 200 - KV store reset successfully
  * @returns {Error}  500 - Failed to reset KV store
  */
-dbRouter.get('/reset-kv', async (req: Request, res: Response) => {
+dbRouter.get('/db/reset', async (req: Request, res: Response) => {
   console.log('[RESET-KV] Starting KV store reset');
 
   try {
@@ -37,5 +38,52 @@ dbRouter.get('/reset-kv', async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Failed to reset KV store' });
   }
 });
+
+dbRouter.get('/db/view', async (req: Request, res: Response) => {
+  console.log('[RESET-KV] Starting KV store reset');
+
+  try {
+    const data: {
+      users: User[],
+      appointments: Appointment[],
+      contacts: Contact[],
+      services: Service[],
+    } = {
+      users: [],
+      appointments: [],
+      contacts: [],
+      services: [],
+    }
+
+    const entries = Promise.all([
+      kv.get<User>(["users"]),
+      kv.get<Appointment>(["appointments"]),
+      kv.get<Contact>(["contacts"]),
+      kv.get<Service>(["services"]),
+    ])
+
+    for (const entry of await entries) {
+      if (entry.value) {
+        for (const key of Object.keys(entry.value)) {
+          if (key in data) {
+            // deno-lint-ignore no-explicit-any
+            (data[key as keyof typeof data] as any[]).push(entry.value[key]);
+          }
+        }
+      }
+    }
+
+    console.log('[DB-VIEW] KV store fetched successfully');
+
+    res.send({ 
+      message: 'KV store fetched successfully',
+      data,
+     });
+  } catch (error) {
+    console.error(`[DB-VIEW] Error fetching KV store: ${error}`);
+    res.status(500).json({ error: 'Failed to fetch KV store' });
+  }
+});
+
 
 export { dbRouter };
